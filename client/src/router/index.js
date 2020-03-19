b4b4c61c161915772012236068a5eab92517cf01
afif julio <afifjulio28@gmail.com>
2020-03-19 21:03:06 +0700

    creating game rules template

--- client/src/router/index.js
+++ client/src/router/index.js
@@ -2,6 +2,7 @@ import Vue from 'vue'
 import VueRouter from 'vue-router'
 // import Home from '../views/Home.vue'
 import login from '../views/login'
+import GameRules from '../views/GameRules.vue'
 
 Vue.use(VueRouter)
 
@@ -25,7 +26,19 @@ const routes = [
     // this generates a separate chunk (about.[hash].js) for this route
     // which is lazy-loaded when the route is visited.
     component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
-  }
+  },
+  {
+    path: '/rules',
+    name: 'GameRules',
+    component: GameRules,
+    // beforeEnter(to, from, next) {
+    //   if (!localStorage.token) {
+    //     next();
+    //   } else {
+    //     next({ name: 'lobby' });
+    //   }
+    // }
+  },
 ]
 
 const router = new VueRouter({
---
+++ client/src/views/GameRules.vue
@@ -0,0 +1,27 @@
+<template>
+<div>
+    <div class="d-flex justify-content-center align-items-center">
+        <div class="card text-white bg-info mb-3 " style="max-width: 18rem;">
+        <div class="card-header text-center">How To Play The Game</div>
+        <div class="card-body">
+            <ul class="list-group">
+                <li class="list-group-item">1. On the login page, pickup the username </li>
+                <li class="list-group-item">2. Pick the room game. this game has 2 maximum player</li>
+                <li class="list-group-item">3. On the battle page, you will see a card with a word in it.</li>
+                <li class="list-group-item">4. Type the word as fast as you can! don't let the enemy defeats you!</li>
+                <li class="list-group-item">5. The winner is decided by who has the biggest HP in the end of the game!</li>
+                <li class="list-group-item">6. Have fun and enjoy the game!</li>
+            </ul>
+        </div>
+        </div>
+    </div>
+</div>
+</template>
+<script>
+export default {
+    name:'GameRules',
+}
+</script>
+<style scoped>
+
+</style>
