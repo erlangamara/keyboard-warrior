5ace3eeb3fb4f5862de835076bf08b0ed0eb6a06
afif julio <afifjulio28@gmail.com>
2020-03-19 16:15:09 +0700

    creating card template

--- client/src/components/wordCard.vue
+++ client/src/components/wordCard.vue
@@ -1,5 +1,14 @@
 <template>
-    
+<div>
+    <div class="card text-center">
+        <div class="card-header">
+            Lets Start The Game!
+        </div>
+        <div class="card-body">
+            <h5 class="card-title"></h5>
+        </div>
+    </div>
+</div>
 </template>
 <script>
 export default {
