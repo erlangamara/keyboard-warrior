fc31f7dd64ec7e3c1a964c1fb056dfa4571287d4
afif julio <afifjulio28@gmail.com>
2020-03-19 13:55:50 +0700

    get random words from 3rd party API on server side

--- server/controllers/controllers.js
+++ server/controllers/controllers.js
@@ -38,7 +38,16 @@ class UserController{
     }
 
     static getWords(req,res,next){
-        
+        axios({
+            method: 'GET',
+            url: 'https://random-word-api.herokuapp.com/word?number=1'
+        })
+        .then((result) =>{
+            res.status(200).json({"words": result.data})
+        })
+        .catch(error =>{
+            next(error)
+        })
     }
 
     static attack(req,res,next){
