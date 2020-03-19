9c940b1963f7a0be3e16bb8b33fda012f859f92e
lifegrain <terrykho@ymail.com>
2020-03-19 15:06:32 +0700

    Login front to back done

--- server/config/config.js
+++ server/config/config.js
@@ -1,25 +1,23 @@
 module.exports = {
   "development": {
-    "username": process.env.DB_USERNAME,
-    "password": process.env.DB_PASSWORD,
+    "username": "postgres",
+    "password": "postgres",
     "database": "keyboard_warrior",
     "host": "127.0.0.1",
-    "dialect": "mysql"
+    "dialect": "postgres"
   },
   "test": {
     "username": "root",
     "password": null,
     "database": "database_test",
     "host": "127.0.0.1",
-    "dialect": "mysql",
-    "operatorsAliases": false
+    "dialect": "postgres"
   },
   "production": {
     "username": "root",
     "password": null,
     "database": "database_production",
     "host": "127.0.0.1",
-    "dialect": "mysql",
-    "operatorsAliases": false
+    "dialect": "postgres"
   }
 }
